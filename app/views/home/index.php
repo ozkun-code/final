2a041a5487c53d18b5c017ade9d386f03346d450
Moehid23 <muthadoabdul23@gmail.com>
2023-12-05 20:26:06 +0700

    ini landing page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,10 +11,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Mediplus - Free Medical and Doctor Directory HTML Template.</title>
+    <title>SEECARE</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="../../../public/assets/img/img/favicon.png">
+    <link rel="icon" href="../../../public/assets/img/img/logoweb1__1_-removebg-preview.png">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
@@ -26,7 +26,7 @@
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="../../../public/assets/css/css/font-awesome.min.css">
     <!-- icofont CSS -->
-    <link rel="stylesheet" href="../../../public/assets/css/css/icofont.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/icofont@1.0.0/dist/icofont.min.css">
     <!-- Slicknav -->
     <link rel="stylesheet" href="../../../public/assets/css/css/slicknav.min.css">
     <!-- Owl Carousel CSS -->
@@ -63,50 +63,8 @@
     </div>
     <!-- End Preloader -->
 
-    <!-- Get Pro Button -->
-    <ul class="pro-features">
-        <a class="get-pro" href="#">Get Pro</a>
-        <li class="big-title">Pro Version Available on Themeforest</li>
-        <li class="title">Pro Version Features</li>
-        <li>2+ premade home pages</li>
-        <li>20+ html pages</li>
-        <li>Color Plate With 12+ Colors</li>
-        <li>Sticky Header / Sticky Filters</li>
-        <li>Working Contact Form With Google Map</li>
-        <div class="button">
-            <a href="http://preview.themeforest.net/item/mediplus-medical-and-doctor-html-template/full_screen_preview/26665910?_ga=2.145092285.888558928.1591971968-344530658.1588061879" target="_blank" class="btn">Pro Version Demo</a>
-            <a href="https://themeforest.net/item/mediplus-medical-and-doctor-html-template/26665910" target="_blank" class="btn">Buy Pro Version</a>
-        </div>
-    </ul>
-
     <!-- Header Area -->
     <header class="header">
-        <!-- Topbar -->
-        <div class="topbar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-5 col-12">
-                        <!-- Contact -->
-                        <ul class="top-link">
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Doctors</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">FAQ</a></li>
-                        </ul>
-                        <!-- End Contact -->
-                    </div>
-                    <div class="col-lg-6 col-md-7 col-12">
-                        <!-- Top Contact -->
-                        <ul class="top-contact">
-                            <li><i class="fa fa-phone"></i>+880 1234 56789</li>
-                            <li><i class="fa fa-envelope"></i><a href="mailto:support@yourmail.com">support@yourmail.com</a></li>
-                        </ul>
-                        <!-- End Top Contact -->
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Topbar -->
         <!-- Header Inner -->
         <div class="header-inner">
             <div class="container">
@@ -115,7 +73,7 @@
                         <div class="col-lg-3 col-md-3 col-12">
                             <!-- Start Logo -->
                             <div class="logo">
-                                <a href="index.html"><img src="img/logo.png" alt="#"></a>
+                                <a href="index.html"><img src="../../../public/assets/img/img/logoweb1-removebg-preview (1).png" width="100" alt="#"></a>
                             </div>
                             <!-- End Logo -->
                             <!-- Mobile Nav -->
@@ -129,7 +87,7 @@
                                     <ul class="nav menu">
                                         <li class="active"><a href="#">Home <i class="icofont-rounded-down"></i></a>
                                             <ul class="dropdown">
-                                                <li><a href="index.html">Home Page 1</a></li>
+                                                <li><a href="../../../public/assets/404.html">Home Page 1</a></li>
                                             </ul>
                                         </li>
                                         <li><a href="#">Doctos </a></li>
@@ -152,7 +110,7 @@
                         </div>
                         <div class="col-lg-2 col-12">
                             <div class="get-quote">
-                                <a href="appointment.html" class="btn">Book Appointment</a>
+                                <a href="<?= BASEURL; ?>/login" class="btn">Login</a>
                             </div>
                         </div>
                     </div>
