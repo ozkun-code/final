a91b72b48086c9fdd44764b10d08432e27563e3f
Moehid23 <muthadoabdul23@gmail.com>
2024-01-04 19:16:29 +0700

    ui register

--- app/views/layouts/navadmin.php
+++ app/views/layouts/navadmin.php
@@ -1,227 +1,211 @@
 <body>
-    <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
-        <!-- Menu -->
+  <!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- Menu -->
 
-        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-          <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
-              <span class="app-brand-logo demo">
+      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        <div class="app-brand demo">
+          <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
               <img src="<?= BASEURL; ?>/assets/img/icons/icon.svg" width="45" />
-              </span>
-              <span class="app-brand-text demo menu-text fw-bolder ms-2">SI CARE</span>
-            </a>
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">SEECARE</span>
+          </a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-              <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
-          </div>
+          <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+          </a>
+        </div>
 
-          <div class="menu-inner-shadow"></div>
+        <div class="menu-inner-shadow"></div>
 
-          <ul class="menu-inner py-1">
-            <!-- Dashboard -->
-            <li class="menu-item">
-              <a href="index.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
-                <div data-i18n="Analytics">Dashboard Admin</div>
-              </a>
-            </li>
-            <li class="menu-header small text-uppercase">
-              <span class="menu-header-text">KLINIK</span>
-            </li>
-            <!-- Doctors -->
-            <li class="menu-item">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-plus-medical"></i>
-                <div data-i18n="Layouts">Doctors</div>
-                </a>
-                <ul class="menu-sub">
-                <li class="menu-item">
-                    <a href="layouts-without-menu.html" class="menu-link">
-                    <div data-i18n="Without menu">List Of Doctors</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="layouts-without-navbar.html" class="menu-link">
-                    <div data-i18n="Without navbar">Add New Doctors</div>
-                    </a>
-                </li>
-                </ul>
-            </li>
-             <!-- Appointments -->
-             <li class="menu-item">
-            <a href="appointments.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-calendar-plus"></i>
-                <div data-i18n="Appointments">Appointments</div>
+        <ul class="menu-inner py-1">
+          <!-- Dashboard -->
+          <li class="menu-item">
+            <a href="index.html" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-home-circle"></i>
+              <div data-i18n="Analytics">Dashboard Admin</div>
             </a>
-            </li>
-             <!-- Appointments list -->
-             <li class="menu-item">
-            <a href="appointments.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-calendar"></i>
-                <div data-i18n="Appointments">Appointments List</div>
-            </a>
-            </li>
-
-                            <!-- Patients -->
-            <li class="menu-item">
+          </li>
+          <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">KLINIK</span>
+          </li>
+          <!-- Doctors -->
+          <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-user"></i>
-                <div data-i18n="Layouts">Patients</div>
+              <i class="menu-icon tf-icons bx bx-plus-medical"></i>
+              <div data-i18n="Layouts">Doctors</div>
+            </a>
+            <ul class="menu-sub">
+              <li class="menu-item">
+                <a href="layouts-without-menu.html" class="menu-link">
+                  <div data-i18n="Without menu">List Of Doctors</div>
+                </a>
+              </li>
+              <li class="menu-item">
+                <a href="layouts-without-navbar.html" class="menu-link">
+                  <div data-i18n="Without navbar">Add New Doctors</div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- Appointments -->
+          <li class="menu-item">
+            <a href="appointments.html" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-calendar-plus"></i>
+              <div data-i18n="Appointments">Appointments</div>
+            </a>
+          </li>
+          <!-- Appointments list -->
+          <li class="menu-item">
+            <a href="appointments.html" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-calendar"></i>
+              <div data-i18n="Appointments">Appointments List</div>
+            </a>
+          </li>
+
+          <!-- Patients -->
+          <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+              <i class="menu-icon tf-icons bx bx-user"></i>
+              <div data-i18n="Layouts">Patients</div>
             </a>
 
             <ul class="menu-sub">
-                <li class="menu-item">
+              <li class="menu-item">
                 <a href="layouts-without-menu.html" class="menu-link">
-                    <div data-i18n="Without menu">List Of Patients</div>
+                  <div data-i18n="Without menu">List Of Patients</div>
                 </a>
-                </li>
-                <li class="menu-item">
+              </li>
+              <li class="menu-item">
                 <a href="layouts-without-navbar.html" class="menu-link">
-                    <div data-i18n="Without navbar">Add New Patients</div>
+                  <div data-i18n="Without navbar">Add New Patients</div>
                 </a>
-                </li>    
+              </li>
             </ul>
-            </li>
-            <!-- Doctors -->
-            <li class="menu-item">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-plus-medical"></i>
-                <div data-i18n="Layouts">Drug</div>
-                </a>
-                <ul class="menu-sub">
-                <li class="menu-item">
-                    <a href="layouts-without-menu.html" class="menu-link">
-                    <div data-i18n="Without menu">List Of Drug</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="layouts-without-navbar.html" class="menu-link">
-                    <div data-i18n="Without navbar">Add New Drug</div>
-                    </a>
-                </li>
-                </ul>
-            </li>
-            <!-- Appointments list -->
-            <li class="menu-item">
-            <a href="appointments.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-notepad"></i>
-                <div data-i18n="Appointments">Prescription</div>
+          </li>
+          <!-- Doctors -->
+          <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+              <i class="menu-icon tf-icons bx bx-plus-medical"></i>
+              <div data-i18n="Layouts">Drug</div>
             </a>
-            </li>
+            <ul class="menu-sub">
+              <li class="menu-item">
+                <a href="layouts-without-menu.html" class="menu-link">
+                  <div data-i18n="Without menu">List Of Drug</div>
+                </a>
+              </li>
+              <li class="menu-item">
+                <a href="layouts-without-navbar.html" class="menu-link">
+                  <div data-i18n="Without navbar">Add New Drug</div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- Appointments list -->
+          <li class="menu-item">
+            <a href="appointments.html" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-notepad"></i>
+              <div data-i18n="Appointments">Prescription</div>
+            </a>
+          </li>
 
-           
-  
-          </ul>
-        </aside>
-        <!-- / Menu -->
 
-        <!-- Layout container -->
-        <div class="layout-page">
-          <!-- Navbar -->
 
-          <nav
-            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-            id="layout-navbar"
-          >
-            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                <i class="bx bx-menu bx-sm"></i>
-              </a>
-            </div>
+        </ul>
+      </aside>
+      <!-- / Menu -->
 
-            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-              <!-- Search -->
-              <div class="navbar-nav align-items-center">
-                <div class="nav-item d-flex align-items-center">
-                  <i class="bx bx-search fs-4 lh-0"></i>
-                  <input
-                    type="text"
-                    class="form-control border-0 shadow-none"
-                    placeholder="Search<?= BASEURL; ?>."
-                    aria-label="Search<?= BASEURL; ?>."
-                  />
-                </div>
+      <!-- Layout container -->
+      <div class="layout-page">
+        <!-- Navbar -->
+
+        <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+          <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+              <i class="bx bx-menu bx-sm"></i>
+            </a>
+          </div>
+
+          <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <!-- Search -->
+            <div class="navbar-nav align-items-center">
+              <div class="nav-item d-flex align-items-center">
+                <i class="bx bx-search fs-4 lh-0"></i>
+                <input type="text" class="form-control border-0 shadow-none" placeholder="Search<?= BASEURL; ?>." aria-label="Search<?= BASEURL; ?>." />
               </div>
-              <!-- /Search -->
+            </div>
+            <!-- /Search -->
 
-              <ul class="navbar-nav flex-row align-items-center ms-auto">
-                <!-- Place this tag where you want the button to render. -->
-                <li class="nav-item lh-1 me-3">
-                  <a
-                    class="github-button"
-                    href="https://github.com/themeselection/sneat-html-admin-template-free"
-                    data-icon="octicon-star"
-                    data-size="large"
-                    data-show-count="true"
-                    aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
-                    >Star</a
-                  >
-                </li>
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
+              <!-- Place this tag where you want the button to render. -->
+              <li class="nav-item lh-1 me-3">
+                <a class="github-button" href="https://github.com/themeselection/sneat-html-admin-template-free" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-admin-template-free on GitHub">Star</a>
+              </li>
 
-                <!-- User -->
-                <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                    <div class="avatar avatar-online">
-                      <img src="<?= BASEURL; ?>/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                    </div>
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <div class="d-flex">
-                          <div class="flex-shrink-0 me-3">
-                            <div class="avatar avatar-online">
-                              <img src="<?= BASEURL; ?>/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                            </div>
-                          </div>
-                          <div class="flex-grow-1">
-                            <span class="fw-semibold d-block">John Doe</span>
-                            <small class="text-muted">Admin</small>
+              <!-- User -->
+              <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                  <div class="avatar avatar-online">
+                    <img src="<?= BASEURL; ?>/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                  </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <div class="d-flex">
+                        <div class="flex-shrink-0 me-3">
+                          <div class="avatar avatar-online">
+                            <img src="<?= BASEURL; ?>/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
                           </div>
                         </div>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <i class="bx bx-user me-2"></i>
-                        <span class="align-middle">My Profile</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <i class="bx bx-cog me-2"></i>
-                        <span class="align-middle">App Settings</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <span class="d-flex align-items-center align-middle">
-                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                          <span class="flex-grow-1 align-middle">Billing</span>
-                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="auth-login-basic.html">
-                        <i class="bx bx-power-off me-2"></i>
-                        <span class="align-middle">Log Out</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <!--/ User -->
-              </ul>
-            </div>
-          </nav>
+                        <div class="flex-grow-1">
+                          <span class="fw-semibold d-block">John Doe</span>
+                          <small class="text-muted">Admin</small>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="bx bx-user me-2"></i>
+                      <span class="align-middle">My Profile</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="bx bx-cog me-2"></i>
+                      <span class="align-middle">App Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <span class="d-flex align-items-center align-middle">
+                        <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                        <span class="flex-grow-1 align-middle">Billing</span>
+                        <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="auth-login-basic.html">
+                      <i class="bx bx-power-off me-2"></i>
+                      <span class="align-middle">Log Out</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <!--/ User -->
+            </ul>
+          </div>
+        </nav>
 
-          <!-- / Navbar -->
+        <!-- / Navbar -->
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,13 +1,164 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
 <head>
-    <title>Register</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Register Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="https://www.rapidexpress.pk/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="https://www.rapidexpress.pk/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="https://www.rapidexpress.pk/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="https://www.rapidexpress.pk/js/config.js"></script>
 </head>
+
 <body>
-    <form method="POST" action="<?= BASEURL; ?>/register/process">
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
-    <input type="submit" value="Register">
-</form>
+    <!-- Content -->
+
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register Card -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">
+                                    <img src="<?= BASEURL; ?>/assets/img/icons/icon.svg" width="50" />
+                                </span>
+                                <span class="app-brand-text demo text-body fw-bolder">SEECARE</span>
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <h4 class="mb-2">Register here 🚀</h4>
+                        <?php Flasher::flash(); ?>
+
+                        <div class="card-body">
+                            <form action="<?= BASEURL; ?>/patients/createactive" method="post">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="first_name">First Name:</label>
+                                            <input type="text" class="form-control" id="first_name" name="first_name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="last_name">Last Name:</label>
+                                            <input type="text" class="form-control" id="last_name" name="last_name" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="email">Email:</label>
+                                    <input type="email" class="form-control" id="email" name="email" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="password">Password:</label>
+                                    <input type="password" class="form-control" id="password" name="password" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="password">Confirm Password:</label>
+                                    <input type="password" class="form-control" id="password" name="password" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="gender">Gender:</label><br>
+                                    <label>Male</label>
+                                    <input type="radio" name="gender" id="male" value="male" checked>
+                                    <label>Female</label>
+                                    <input type="radio" name="gender" id="female" value="female">
+                                    </fieldset>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="contact">Contact:</label>
+                                    <input type="text" class="form-control" id="contact" name="contact" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="address">Address:</label>
+                                    <textarea class="form-control" id="address" name="address" required></textarea><br>
+                                    <div class="mb-3">
+                                        <label class="form-label" for="date_of_birth">date 0f birth:</label><br>
+                                        <input id="date_of_birth" name="date_of_birth" type="date">
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
+                        <div class="mb-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                                <label class="form-check-label" for="terms-conditions">
+                                    I agree to
+                                    <a href="javascript:void(0);">privacy policy & terms</a>
+                                </label>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary d-grid w-100">Sign up</button>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Already have an account?</span>
+                            <a href="<?= BASEURL; ?>/login">
+                                <span>Sign in instead</span>
+                            </a>
+                        </p>
+
+                    </div>
+                    <!-- Register Card -->
+                </div>
+            </div>
+        </div>
+
+        <!-- / Content -->
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="https://www.rapidexpress.pk/vendor/libs/jquery/jquery.js"></script>
+        <script src="https://www.rapidexpress.pk/vendor/libs/popper/popper.js"></script>
+        <script src="https://www.rapidexpress.pk/vendor/js/bootstrap.js"></script>
+        <script src="https://www.rapidexpress.pk/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+        <script src="https://www.rapidexpress.pk/vendor/js/menu.js"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+
+        <!-- Main JS -->
+        <script src="https://www.rapidexpress.pk/js/main.js"></script>
+
+        <!-- Page JS -->
+
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+</body>
+
+</html>
